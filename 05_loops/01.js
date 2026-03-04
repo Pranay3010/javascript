09f8c897a5b6aaa05d62269758c912dcc0ae9ae2
Pranay3010 <pranayrathore30@gmail.com>
2026-03-04 17:28:59 +0000

    loops

---
+++ 05_loops/01.js
@@ -0,0 +1,57 @@
+// for loop
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);
+//     if (i ==5) {
+//         console.log("5 is multiplied by 2");
+//     }
+// }
+
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <=10; j++) {
+//         console.log(`Inner loop value: ${j}`);
+        
+//     }
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <=10; j++) {
+//         // console.log(`Inner loop value: ${j}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+
+//     }
+// }
+
+// let myArray = ["flask", "django", "express", "spring"];
+// console.log(myArray.length);
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i]);
+// }
+
+
+// break and continue
+
+// for (let i = 1; i <=20; i++) {
+//     if (i == 5) {
+//         console.log("5 is detected");
+//         break;
+//     }
+// console.log(i);
+// }
+
+
+for (let i = 1; i <=20; i++) {
+    if (i == 5) {
+        console.log("5 is detected");
+        continue;
+    }
+console.log(i);
+}
