fc0708a5a9479385b2fcc474c770d63f6300b97d
Pranay3010 <pranayrathore30@gmail.com>
2026-03-05 12:53:15 +0000

    while

---
+++ 05_loops/02_while.js
@@ -0,0 +1,39 @@
+// while
+
+// let i=0 ;    // initalization
+// while (i <= 10) {   // condition
+//     console.log(i);
+//     i++;    // update
+// }
+
+
+
+
+
+// let index = 0;
+// while (index < 10){
+//     console.log(index);
+//     index = index + 2;
+// }
+
+// let myArray = ["flask", "django", "express", "spring"];
+
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+
+//+++++++++++++ do while
+
+// let score = 0;
+// do {
+//     console.log("Your score is: " + score);
+//     score++;
+// } while (score < 0);
+
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter your name: ");
+console.log("Hello " + name);
