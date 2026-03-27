c4fcc31d77862d430a2bb5150b16eec021f3f1ac
Pranay3010 <pranayrathore30@gmail.com>
2026-03-27 05:20:24 +0000

    new

--- 05_loops/08.js
+++ 05_loops/08.js
@@ -1,12 +1,12 @@
-// reduce 
+reduce 
 
-// const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5];
 
-// const sum = arr.reduce((acc, curr) => { 
-//     console.log(`acc: ${acc}, curr: ${curr}`);
-//     return acc + curr; }, 0); // 0 is the initial value of acc
+const sum = arr.reduce((acc, curr) => { 
+    console.log(`acc: ${acc}, curr: ${curr}`);
+    return acc + curr; }, 0); // 0 is the initial value of acc
 
-// console.log(sum); 
+console.log(sum); 
 
 const shop =[
     {name: "laptop", price: 1000},
@@ -23,3 +23,5 @@ let totalPrice = shop.reduce((acc, item) => {
 }, 0);
 
 console.log(totalPrice);
+
+}
